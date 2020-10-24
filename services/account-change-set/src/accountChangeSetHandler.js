3fa8b7ed6abcdf853d8294a264f991fdaf213241
silvamb <matheus-b-silva@outlook.com>
2020-10-24 21:26:20 +0100

    Fix IAM permissions and code (#38)

--- services/account-change-set/src/accountChangeSetHandler.js
+++ services/account-change-set/src/accountChangeSetHandler.js
@@ -1,7 +1,7 @@
 const { saveChangeSet, ChangeSet } = require('libs/version');
 exports.saveChangeSet = async (dynamodb, event) => {
 
-    const {accountId, version, changeSet} = JSON.parse(event.detail);
+    const {accountId, version, changeSet} = event.detail;
 
-    saveChangeSet(dynamodb, new ChangeSet(accountId, version, changeSet));
+    return saveChangeSet(dynamodb, new ChangeSet(accountId, version, changeSet));
 }
--- services/account-change-set/test/testValues.js
+++ services/account-change-set/test/testValues.js
@@ -11,11 +11,11 @@ exports.expectedChangeSet = [{
 exports.createChangeSetEvent = exports.updateExpressionRuleEvent = {
     source:"virtwallet",
     'detail-type': "new account version",
-    detail: JSON.stringify({
+    detail: {
       accountId: this.ACCOUNT_ID,
       version: 10,
       changeSet: this.expectedChangeSet
-    })
+    }
 }
 
 exports.putItemResult = {
